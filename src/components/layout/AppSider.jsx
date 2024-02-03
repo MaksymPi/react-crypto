f566f738424c07efc22cf5809a256604f50c2476
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-02-03 12:23:21 +0100

    statistic

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react'
 
-import { Layout, Card, Statistic, List, Typography, Spin  } from 'antd';
+import { Layout, Card, Statistic, List, Typography, Spin } from 'antd';
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+
+import uuid from 'react-uuid';
 import { FetchData, FetchAssets } from '../../api';
+import { percentDifference } from '../../utils/'
+
 
 const siderStyle = {
     padding: '1rem',
 };
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+
 
 
 
@@ -23,46 +21,62 @@ const AppSider = () => {
     const [crypto, setCrypto] = useState([])
     const [assets, setAssets] = useState([])
 
-    useEffect(() =>{
+    useEffect(() => {
         async function preload() {
             setLoading(true)
             const { result } = await FetchData();
             const assets = await FetchAssets();
 
+
+            setAssets(assets.map(asset => {
+                const coin = result.find((c) => c.id === asset.id)
+                return {
+                    grow: asset.price < coin.price,
+                    growPercent: percentDifference(asset.price, coin.price),
+                    totalAmount: asset.amount * coin.price,
+                    totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+                    ...asset
+                }
+            }))
             setCrypto(result)
-            setAssets(assets)
             setLoading(false)
         }
         preload()
     }, [])
 
     if (loading) {
-       return <Spin fullscreen />
+        return <Spin fullscreen />
     }
 
     return (<Layout.Sider width="25%" style={siderStyle}>
-        <Card style={{ marginBottom: '1rem' }}>
-            <Statistic
-                title="Active"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                    color: '#3f8600',
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"
-            />
-            <List
-                size='small'
-                dataSource={data}
-                renderItem={(item) => (
-                    <List.Item>
-                        <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                    </List.Item>
-                )}
-            />
-        </Card>
-        <Card>
+        {assets.map(asset => (
+            <Card key={uuid()} style={{ marginBottom: '1rem' }}>
+                <Statistic
+                    title={asset.id}
+                    value={asset.totalAmount}
+                    precision={2}
+                    valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322'}}
+                    prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+                    suffix="$"
+                />
+                <List
+                    size='small'
+                    dataSource={[
+                        {title: 'Total Profit', value: asset.totalProfit},
+                        {title: 'Asset Amount', value: asset.amount},
+                        {title: 'Difference', value: asset.growPercent},
+                    ]}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <span>{item.title}</span>
+                            <span>{item.value}</span>
+                        </List.Item>
+                    )}
+                />
+            </Card>
+        ))}
+
+        {/* <Card>
             <Statistic
                 title="Idle"
                 value={9.3}
@@ -73,7 +87,7 @@ const AppSider = () => {
                 prefix={<ArrowDownOutlined />}
                 suffix="%"
             />
-        </Card>
+        </Card> */}
     </Layout.Sider>
     )
 }
